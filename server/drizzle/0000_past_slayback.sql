b98e6b9174c59666dc3519e71873ad964afcfc8e
para7 <cometpara7@gmail.com>
2025-03-28 14:00:27 +0000

    feat: drizzle,d1と接続成功

---
+++ server/drizzle/0000_past_slayback.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `users` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text NOT NULL,
+	`age` integer NOT NULL
+);
--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,12 @@
 // server/index.ts
 import { Hono } from "hono";
+import { drizzle } from "drizzle-orm/d1";
+import { usersTable } from "./db/schema";
 
 const app = new Hono<{
   Bindings: {
     MY_VAR: string;
+    DB: D1Database;
   };
   Variables: {
     MY_VAR_IN_VARIABLES: string;
@@ -16,10 +19,21 @@ app.use(async (c, next) => {
   c.header("X-Powered-By", "React Router and Hono");
 });
 
-app.get("/api", (c) => {
+app.get("/api", async (c) => {
+  const db = drizzle(c.env.DB);
+
+  const writeResult = await db.insert(usersTable).values({
+    name: "John Doe",
+    age: Math.floor(Math.random() * 100),
+  });
+  console.log(writeResult);
+  const result = await db.select().from(usersTable);
+  console.log(result);
+
   return c.json({
     message: "Hello",
     var: c.env.MY_VAR,
+    users: result,
   });
 });
 
