fc6847d82ec845f586aeb7993ef6cf50c20f1163
para7 <cometpara7@gmail.com>
2025-03-29 04:42:36 +0000

    remix -> d1 疎通OK

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,15 +1,21 @@
+import { drizzle } from "drizzle-orm/d1";
 import type { Route } from "./+types/_index";
+import { usersTable } from "server/db/schema";
 
-export const loader = (args: Route.LoaderArgs) => {
+export const loader = async (args: Route.LoaderArgs) => {
 	const extra = args.context.extra;
 	const cloudflare = args.context.cloudflare;
 	const myVarInVariables = args.context.hono.context.get("MY_VAR_IN_VARIABLES");
 	const isWaitUntilDefined = !!cloudflare.ctx.waitUntil;
-	return { cloudflare, extra, myVarInVariables, isWaitUntilDefined };
+
+	const db = drizzle(cloudflare.env.DB);
+	const result = await db.select().from(usersTable);
+
+	return { cloudflare, extra, myVarInVariables, isWaitUntilDefined, result };
 };
 
 export default function Index({ loaderData }: Route.ComponentProps) {
-	const { cloudflare, extra, myVarInVariables, isWaitUntilDefined } =
+	const { cloudflare, extra, myVarInVariables, isWaitUntilDefined, result } =
 		loaderData;
 	return (
 		<div>
@@ -23,6 +29,7 @@ export default function Index({ loaderData }: Route.ComponentProps) {
 			<h4>Extra is {extra}</h4>
 			<h5>Var in Variables is {myVarInVariables}</h5>
 			<h6>waitUntil is {isWaitUntilDefined ? "defined" : "not defined"}</h6>
+			<pre>{JSON.stringify(result, null, 2)}</pre>
 		</div>
 	);
 }
